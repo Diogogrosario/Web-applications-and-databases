149652798b23c6d4145bf6684efa2f30deeeb0a3
up201806531 <up201806531@fe.up.pt>
2021-02-28 20:30:20 +0000

    Added signup page. added sign in button to signin page. Both pages now work better with smaller sizes and when changing sizes.

--- signin.php
+++ signin.php
@@ -5,22 +5,25 @@
 
     <!-- Form -->
     <div>
-        <div class="col-md-6 offset-md-3" style="padding-top: 3%;">
+        <div style="margin: auto; width: 60%; padding-top: 3%; font-size: 1.2rem;">
             <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Username</b></label>
             <input type="text" class="form-control" id="formGroupExampleInput" placeholder="username">
         </div>
-        <div class="col-md-6 offset-md-3" style="padding-top: 2%;">
+        <div style="margin: auto; width: 60%; padding-top: 2%; font-size: 1.2rem;">
             <label style="padding-bottom: 1%;" for="formGroupExampleInput2" class="form-label"><b>Password</b></label>
             <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="password">
         </div>
-        <div class="offset-md-3" style="padding-top: 1%;">
+        <div style="margin: auto; width: 60%; padding-top: 1%;">
             <a href="#Forgot-password">Forgot password?</a>
+        </div>
+        <div style="text-align: center;">
+            <button type="button" class="btn btn-primary" style="margin-top: 5%; font-size: 2em;">Sign in</button>
         </div>
         <div style="text-align: center;">
             <p style="padding-top: 5%; margin-bottom: 0;">Don't have an account yet</p>
         </div>
-        <div style="text-align: center;">
-            <a href="#Sign-in">Sign in</p>
+        <div style="text-align: center;  font-size: 1.3rem;">
+            <a href="#Sign-in">Sign up</p>
         </div>
     </div>
     <?php include_once('footer.html'); ?>
