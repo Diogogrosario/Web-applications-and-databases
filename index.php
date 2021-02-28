7bd3f201f55bf1f45e523a2453b5f2562d92ab1a
Henrique <gamingwithapotato@gmail.com>
2021-02-28 18:06:37 +0000

    Added colapsable side menu

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include_once('header.html'); ?>
 
 <div class="row no-gutters">
-    <div class="col-lg-2  col-sm-3">
-        <div class="side">
+    <div class="col-lg-2  col-sm-2 collapse" id="collapsableColumn" >
+        <div class="side" >
             <div class="list-group">
                 <a href="#" class="list-group-item list-group-item-action">Computers</a>
                 <a href="#" class="list-group-item list-group-item-action">Books</a>
@@ -11,7 +11,10 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-10 col-sm-9">
+    <button class="btn btn-primary " type="button" data-bs-toggle="collapse" data-bs-target="#collapsableColumn" aria-expanded="false" aria-controls="collapsableColumn" style="width: 2%; min-height: 90vh; height: 100%;">
+        >
+    </button>
+    <div class="col">
         <div class="content">
             <div class="row">
                 <p class="fs-1">Section 1</p>
