7970db6277ecccd8bfaf56a1e8b23d5e5d05b643
up201806531 <up201806531@fe.up.pt>
2021-03-01 00:08:11 +0000

    Added about page

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php include_once('header.html'); ?>
+
+    <div id="gridContainer" style="display: grid; grid-template-rows: 35% 30% 20% 20%">
+        <div style="text-align: center; font-size: 2.5rem; grid-row: 1">
+            <b>About us</b>
+        </div>
+        <div style=" font-size: 1.3rem; padding: 2%; grid-row: 2">
+            <a>In case you need some sort of help, here are some useful links:<ab>
+        </div>
+        <div style=" font-size: 1.3rem; padding: 2%; grid-row: 3">
+            <a href="#faq">FAQ:<ab>
+        </div>
+        <div style=" font-size: 1.3rem; padding: 2%; grid-row: 4">
+            <a href="#contacts">Contacts/Locations:<ab>
+        </div>
+    </div>
+
+<?php include_once('footer.html'); ?>
