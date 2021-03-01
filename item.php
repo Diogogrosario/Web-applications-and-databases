4b67f73d2e787a741177ab1b9f3d375cc9a0492f
davide-castro20 <davideafc@gmail.com>
2021-03-01 23:08:41 +0000

    Added nav tabs to item page

--- item.php
+++ item.php
@@ -53,9 +53,9 @@
             </div>
 
             <div class="col pt-4">
-                <div class="row ps-5 pb-5">
-                    <h1>Aquele jogo 2: A Sequela</h1>
-                    <h3 class="ps-5">Videogame</h3>
+                <div class="row ps-5 pb-5 pt-2">
+                    <h1 id="productName">Cyberpunk 2077: Day One Edition</h1>
+                    <!-- <h4 class="ps-5" id="productCategory">Videogame</h4> -->
                 </div>
                 <div class="row">
                     <div class="col-7">
@@ -67,13 +67,13 @@
                             </div>
                             <div class="carousel-inner" style="width:100%; height:40vh">
                                 <div class="carousel-item active">
-                                    <img src="images/shrek2PS2.png" class="d-block img-fluid mx-auto" alt="Shrek" style="max-height:40vh;">
+                                    <img src="images/cyberpunk_1.jpg" class="d-block img-fluid mx-auto" alt="Cyberpunk1" style="max-height:40vh;">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="images/shrek2PS2_1.png" class="d-block img-fluid mx-auto" alt="Shrek" style="max-height:40vh;">
+                                    <img src="images/cyberpunk_2.jpg" class="d-block img-fluid mx-auto" alt="Cyberpunk2" style="max-height:40vh;">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="images/shrek2PS2_2.png" class="d-block img-fluid mx-auto" alt="Shrek" style="max-height:40vh;">
+                                    <img src="images/cyberpunk_3.jpg" class="d-block img-fluid mx-auto" alt="Cyberpunk3" style="max-height:40vh;">
                                 </div>
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductImages" data-bs-slide="prev">
@@ -100,26 +100,92 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col" id="buySection">
-                                <div class="fs-1" id="productPrice" style="color:red;">
+                            <div class="col ps-5" id="buySection">
+                                <div class="" id="productPrice" style="color:red; font-size:3em;">
                                     10.99 €
                                 </div>
-                                <button class="btn btn-success">Add to Cart</button>
+                                <button class="btn btn-success mt-4">Add to Cart</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row mt-4">
-                    <div class="col-7 text-center">
-                        <section id="productDetails">
-                            <h2>Product Details</h2>
-                        </section>
+
+
+
+                <div class="row mt-5">
+                    <nav>
+                        <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
+                            <button class="nav-link active" id="nav-description-tab" data-bs-toggle="tab" data-bs-target="#nav-description" type="button" role="tab" aria-controls="nav-description" aria-selected="true">Description</button>
+                            <button class="nav-link" id="nav-details-tab" data-bs-toggle="tab" data-bs-target="#nav-details" type="button" role="tab" aria-controls="nav-details" aria-selected="false">Details</button>
+                            <button class="nav-link" id="nav-reviews-tab" data-bs-toggle="tab" data-bs-target="#nav-reviews" type="button" role="tab" aria-controls="nav-reviews" aria-selected="false">Reviews</button>
+                        </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-description" role="tabpanel" aria-labelledby="nav-description-tab">
+                            <section class="ps-5 pe-5" id="productDescription">
+                                <h3 class="text-start mt-4">Description</h3>
+                                <div class="mt-4" id="descriptionText">
+                                <p align=justify>Cyberpunk 2077 é uma história de ação/aventura no mundo aberto de Night City, uma megalópole obcecada com poder, glamour e alterações de corpos. Aqui serás V, um mercenário exilado que persegue um implante essencial para obter a imortalidade. Poderás personalizar o cyberware, habilidades e estilo da tua personagem e explorar uma vasta cidade onde as escolhas que tomares irão moldar a história e o mundo que te rodeia. </P>
+<P align="justify">JOGA COMO UM MERCENÁRIO EXILADO 
+<P align=justify>Transforma-te num cyberpunk, um mercenário urbano equipado com acessório cibernéticos, e cria a tua lenda nas ruas de Night City. 
+<P align=justify>VIVE NA CIDADE DO FUTURO 
+<P align=justify>Entra num enorme mundo aberto de Night City, um local com visuais, complexidades e profundidades inéditas. 
+<P align=justify>ROUBA O IMPLANTE E OBTÉM A VIDA ETERNA 
+<P align=justify>Aceita a missão mais arriscada da tua vida e procura o implante essencial para obter a imortalidade. 
+<P align=justify>O jogo inclui os seguintes itens físicos: 
+<P align=justify>- Caixa com CD de jogos <BR>- Capa reversível <BR>- Compêndio do Mundo com informações sobre o cenário e os ritos do jogo <BR>- Postais de Night City <BR>- Mapa de Night City <BR>- Autocolantes </P>
+<P align=justify>Brindes digitais incluídos: 
+<P align=justify>- Banda sonora do jogo <BR>- Minilivro de arte com uma seleção de arte do jogo <BR>- Banda desenhada Digital “Cyberpunk 2077:Your voice" <BR>- Sourcebook Cyberpunk de 2020 <BR>- Papeis de parede para computador e telemóvel </P>
+                                </div>
+                            </section>
+                        </div>
+                        <div class="tab-pane fade" id="nav-details" role="tabpanel" aria-labelledby="nav-details-tab">
+                            <!-- <div class="col-7 text-center ps-5 pe-5"> -->
+                                <section id="productDetails" class="row ps-5 pe-5 pt-3">
+                                    <h3 class="text-center mt-3">Product Details</h3>
+                                    
+                                    <div class="col-6" id="detailsTable_1">
+                                        <table class="table mt-3">
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">Release Date</th>
+                                                    <td>14-02-2002</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Platform</th>
+                                                    <td>PlayStation 2</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="col" id="detailsTable_2">
+                                        <table class="table mt-3" id="detailsTable2">
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">Age Restriction</th>
+                                                    <td>6+</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Others</th>
+                                                    <td>This game is extremely buggy, has terrible performance on consoles and low/medium end PC's and is lacking comparing to what was presented before release</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </section>
+                            <!-- </div> -->
+                        </div>
+                        <div class="tab-pane fade" id="nav-reviews" role="tabpanel" aria-labelledby="nav-reviews-tab">
+                            <!-- <div class="col"> -->
+                                <section class="ps-5 pe-5" id="productReviews">
+                                    <h3 class="text-start mt-4">Reviews</h3>
+                                </section>
+                            <!-- </div> -->
+                        </div>
                     </div>
-                    <div class="col">
-                        <section id="productReviews">
-                            <h2>Reviews</h2>
-                        </section>
-                    </div>
+
+
                 </div>
             </div>
 
