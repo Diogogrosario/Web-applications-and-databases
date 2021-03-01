44004e733b760e070d88e98eefa25fc45a48ea1d
Henrique <gamingwithapotato@gmail.com>
2021-03-01 22:38:56 +0000

    Made some improvements to main screen

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
-<?php include_once('header.html'); ?>
+<?php include_once('mainPageHeader.html'); ?>
 <?php include_once('sidebarIndex.html'); ?>
 
 <div class="col">
     <div class="content" style="padding:2%">
         <div class="row">
             <div class="container">
-                <p class="fs-1">Section 1</p>
+                <div id="carouselProductImages" class="carousel carousel-dark slide" data-bs-interval="false" data-bs-touch="true">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner" style="width:100%; height:20vh">
+                        <div class="carousel-item active">
+                            <img src="images/wantMoreSales.jpg" class="img-fluid" alt="More Sales" style='height:20vh; width: 100%; object-fit: contain'>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/shrek2PS2_1.png" class="d-block img-fluid mx-auto" style='height:20vh; width: 100%; object-fit: contain' alt="Shrek" style="max-height:40vh;">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/shrek2PS2_2.png" class="d-block img-fluid mx-auto" style='height:20vh; width: 100%; object-fit: contain' alt="Shrek" style="max-height:40vh;">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductImages" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselProductImages" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <p class="fs-1">PC</p>
                 <div class="row flex-row flex-nowrap">
                     <div class="d-flex flex-row flex-nowrap overflow-auto">
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -19,9 +45,9 @@
                         </div>
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -30,9 +56,9 @@
 
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -40,63 +66,63 @@
                         </div>
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/pc.jpg" class="card-img-top" alt="images/pc.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -105,13 +131,13 @@
                     </div>
                 </div>
                 <hr class="my-5" style="width:90%;margin-left:5%;">
-                <p class="fs-1">Section 2</p>
+                <p class="fs-1">Phones</p>
                 <div class="row flex-row flex-nowrap">
                     <div class="d-flex flex-row flex-nowrap overflow-auto">
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -119,9 +145,9 @@
                         </div>
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -130,9 +156,9 @@
 
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
@@ -140,63 +166,63 @@
                         </div>
 
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="card">
+                            <div class="card border-0 border-right-1">
                                 <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Price.</p>
                                 </div>
