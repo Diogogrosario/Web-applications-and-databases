4134d5701a465e149bbac14589674bad9d98a5e9
up201806531 <up201806531@fe.up.pt>
2021-03-01 22:22:46 +0000

    Started work on cart.php and contacts.php. Removed error in about.php

---
+++ cart.php
@@ -0,0 +1,63 @@
+<?php include_once('header.html'); ?>
+
+
+    <div style="font-size: 2.5rem; grid-row: 1;">
+        <a>Your cart: </a>
+    </div>
+
+    <div class="container pt-4 pb-4 overflow-auto" style="background-color: lightgray; width: 80%; height: 75vh;">
+        <!-- Content here -->
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto w-75 mt-1 mb-1">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <button type="button" class="btn btn-success float-end btn-lg" href="SucessPage">Buy all</button>
+    </div>
+
+
+
+<?php include_once('footer.html'); ?>
---
+++ contacts.php
@@ -0,0 +1,25 @@
+<?php include_once('header.html'); ?>
+
+
+<div id="gridContainer" style="display: grid; grid-template-rows: 35% 30% 20%">
+        <div style="text-align: center; font-size: 2.5rem; grid-row: 1;">
+            <b>Contacts</b>
+        </div>
+        <div style=" font-size: 1.5rem; padding: 2%; grid-row: 2; text-align: center; font-weight: bold;">
+            <a>Customer support</a>
+        </div>
+        <div style="grid-row: 3; display: grid; grid-template-columns: 35% 30% 35%">
+            <div style=" font-size: 1.3rem; grid-col: 1;">
+                <a>ABCSNDKANSKDN: 123-341-324</a>
+            </div>
+            <div style=" font-size: 1.3rem; grid-col: 2;">
+                <a>ABCSNDKANSKDN: 123-341-324</a>
+            </div>
+            <div style=" font-size: 1.3rem; grid-col: 3;">
+                <a>ABCSNDKANSKDN: 123-341-324</a>
+            </div>
+    </div>
+    </div>
+
+
+<?php include_once('footer.html'); ?>
