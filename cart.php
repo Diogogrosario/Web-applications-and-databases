979637cc56a65b07ecea75e379b8249dd86a0754
up201806531 <up201806531@fe.up.pt>
2021-03-02 11:21:55 +0000

    Fixed cart page, mostly

--- cart.php
+++ cart.php
@@ -5,65 +5,63 @@
         <a>Your cart: </a>
     </div>
     
-    <div class="col-12">
-        <div class="container pt-4 pb-4 overflow-auto rounded col-8 offset-md-1" style="background-color: lightgray; max-height: 75vh;">
-            <!-- Content here -->
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
-            </div>
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
-            </div>
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
-            </div>
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
-            </div>
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
-            </div>
-            <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card.</p>
-                    <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
-                    <p class="card-text float-end fs-3">1999000.99€</p>
-                </div>
+    <div class="container pt-4 pb-4 overflow-auto rounded col-lg-9 col-12" style="background-color: lightgray; max-height: 75vh; margin-left: 4%; max-width: 92%;">
+        <!-- Content here -->
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
             </div>
         </div>
-        <div class="container offset-md-9">
-            <div class="col position-absolute position-absolute top-50" style="transform: translateX(35%);text-align: center;">
-                <p class="d-inline" style="font-size:1.5em">Total:</p>
-                <p class="d-inline" style="font-size:1.5em">47:97€</p>
-                <p></p>
-                <button type="button" class="btn btn-success btn-lg">Buy all</button>
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
             </div>
+        </div>
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+        <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card.</p>
+                <a href="#" class="btn btn-danger pt-10" style="display: inline-block;">Remove from cart</a>
+                <p class="card-text float-end fs-3">1999000.99€</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg align-middle">
+        <div class="" style="text-align: center;">
+            <p class="d-inline" style="font-size:1.5em">Total:</p>
+            <p class="d-inline" style="font-size:1.5em">47:97€</p>
+            <p></p>
+            <button type="button" class="btn btn-success btn-lg">Buy all</button>
         </div>
     </div>
     
