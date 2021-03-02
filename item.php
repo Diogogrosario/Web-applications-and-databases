853c831e507e4eead3844963112e2f5408bb05ab
davide-castro20 <davideafc@gmail.com>
2021-03-02 10:55:23 +0000

    Changes to item page

--- item.php
+++ item.php
@@ -6,48 +6,29 @@
 
         <div class="row">
 
-            <div class="col-lg-2 col-sm-2 text-center overflow-auto" style="background-color:beige; height:100%;" id="similarProducts">
+            <div class="d-flex flex-column justify-content-center col-2 text-center p-0" style="background-color:beige; height:100%;" id="similarProducts">
 
-                <h1 class="p-3 text-center fs-5">Similar Products</h1>
+                <h1 class="p-3 text-center fs-5 overflow-hidden">Similar Products</h1>
 
-                <div class="row-sm-2">
-                    <div class="card" style="background:transparent;">
-                        <img src="images/shrek2PS2.png" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+                <div class="card border-0 similarProductCard">
+                    <div class="card-body ps-4 pe-3">
+                        <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Price.</p>
                     </div>
                 </div>
-                <div class="row-sm-2">
-                    <div class="card" style="background:transparent;">
-                        <img src="images/shrek2PS2.png" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+                <div class="card border-0 similarProductCard">
+                    <div class="card-body ps-4 pe-3">
+                        <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Price.</p>
                     </div>
                 </div>
-                <div class="row-sm-2">
-                    <div class="card" style="background:transparent;">
-                        <img src="images/shrek2PS2.png" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row-sm-2">
-                    <div class="card" style="background:transparent;">
-                        <img src="images/shrek2PS2.png" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row-sm-2">
-                    <div class="card" style="background:transparent;">
-                        <img src="images/shrek2PS2.png" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+                <div class="card border-0 similarProductCard">
+                    <div class="card-body ps-4 pe-3">
+                        <img src="images/phone.jpg" class="card-img-top" alt="images/phone.jpg">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Price.</p>
                     </div>
                 </div>
             </div>
@@ -100,11 +81,13 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col ps-5" id="buySection">
-                                <div class="" id="productPrice" style="color:red; font-size:3em;">
+                            <div class="col ps-3" id="buySection">
+                                <div class="ps-4" id="productPrice" style="color:red; font-size:3em;">
                                     10.99 €
                                 </div>
-                                <button class="btn btn-success mt-4">Add to Cart</button>
+                                <button class="btn btn-success mt-4 w-50 rounded-0"><i class="bi bi-cart4"></i> Add to Cart</button>
+                                <br>
+                                <button class="btn btn-success mt-4 w-50 rounded-0"><i class="bi bi-heart"></i> Add to Wishlist</button>
                             </div>
                         </div>
                     </div>
@@ -124,63 +107,63 @@
                         <div class="tab-pane fade show active" id="nav-description" role="tabpanel" aria-labelledby="nav-description-tab">
                             <section class="ps-5 pe-5" id="productDescription">
                                 <h3 class="text-start mt-4">Description</h3>
-                                <div class="mt-4" id="descriptionText">
-                                <p align=justify>Cyberpunk 2077 é uma história de ação/aventura no mundo aberto de Night City, uma megalópole obcecada com poder, glamour e alterações de corpos. Aqui serás V, um mercenário exilado que persegue um implante essencial para obter a imortalidade. Poderás personalizar o cyberware, habilidades e estilo da tua personagem e explorar uma vasta cidade onde as escolhas que tomares irão moldar a história e o mundo que te rodeia. </P>
-<P align="justify">JOGA COMO UM MERCENÁRIO EXILADO 
-<P align=justify>Transforma-te num cyberpunk, um mercenário urbano equipado com acessório cibernéticos, e cria a tua lenda nas ruas de Night City. 
-<P align=justify>VIVE NA CIDADE DO FUTURO 
-<P align=justify>Entra num enorme mundo aberto de Night City, um local com visuais, complexidades e profundidades inéditas. 
-<P align=justify>ROUBA O IMPLANTE E OBTÉM A VIDA ETERNA 
-<P align=justify>Aceita a missão mais arriscada da tua vida e procura o implante essencial para obter a imortalidade. 
-<P align=justify>O jogo inclui os seguintes itens físicos: 
-<P align=justify>- Caixa com CD de jogos <BR>- Capa reversível <BR>- Compêndio do Mundo com informações sobre o cenário e os ritos do jogo <BR>- Postais de Night City <BR>- Mapa de Night City <BR>- Autocolantes </P>
-<P align=justify>Brindes digitais incluídos: 
-<P align=justify>- Banda sonora do jogo <BR>- Minilivro de arte com uma seleção de arte do jogo <BR>- Banda desenhada Digital “Cyberpunk 2077:Your voice" <BR>- Sourcebook Cyberpunk de 2020 <BR>- Papeis de parede para computador e telemóvel </P>
+                                <div class="mt-4 text-justify" id="descriptionText">
+                                    <p>Cyberpunk 2077 é uma história de ação/aventura no mundo aberto de Night City, uma megalópole obcecada com poder, glamour e alterações de corpos. Aqui serás V, um mercenário exilado que persegue um implante essencial para obter a imortalidade. Poderás personalizar o cyberware, habilidades e estilo da tua personagem e explorar uma vasta cidade onde as escolhas que tomares irão moldar a história e o mundo que te rodeia. </P>
+                                    <P>JOGA COMO UM MERCENÁRIO EXILADO
+                                    <P>Transforma-te num cyberpunk, um mercenário urbano equipado com acessório cibernéticos, e cria a tua lenda nas ruas de Night City.
+                                    <P>VIVE NA CIDADE DO FUTURO
+                                    <P>Entra num enorme mundo aberto de Night City, um local com visuais, complexidades e profundidades inéditas.
+                                    <P>ROUBA O IMPLANTE E OBTÉM A VIDA ETERNA
+                                    <P>Aceita a missão mais arriscada da tua vida e procura o implante essencial para obter a imortalidade.
+                                    <P>O jogo inclui os seguintes itens físicos:
+                                    <P>- Caixa com CD de jogos <BR>- Capa reversível <BR>- Compêndio do Mundo com informações sobre o cenário e os ritos do jogo <BR>- Postais de Night City <BR>- Mapa de Night City <BR>- Autocolantes </P>
+                                    <P>Brindes digitais incluídos:
+                                    <P>- Banda sonora do jogo <BR>- Minilivro de arte com uma seleção de arte do jogo <BR>- Banda desenhada Digital “Cyberpunk 2077:Your voice" <BR>- Sourcebook Cyberpunk de 2020 <BR>- Papeis de parede para computador e telemóvel </P>
                                 </div>
                             </section>
                         </div>
                         <div class="tab-pane fade" id="nav-details" role="tabpanel" aria-labelledby="nav-details-tab">
                             <!-- <div class="col-7 text-center ps-5 pe-5"> -->
-                                <section id="productDetails" class="row ps-5 pe-5 pt-3">
-                                    <h3 class="text-center mt-3">Product Details</h3>
-                                    
-                                    <div class="col-6" id="detailsTable_1">
-                                        <table class="table mt-3">
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">Release Date</th>
-                                                    <td>14-02-2002</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Platform</th>
-                                                    <td>PlayStation 2</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    <div class="col" id="detailsTable_2">
-                                        <table class="table mt-3" id="detailsTable2">
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">Age Restriction</th>
-                                                    <td>6+</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Others</th>
-                                                    <td>This game is extremely buggy, has terrible performance on consoles and low/medium end PC's and is lacking comparing to what was presented before release</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                            <section id="productDetails" class="row ps-5 pe-5 pt-3">
+                                <h3 class="text-center mt-3">Product Details</h3>
 
-                                </section>
+                                <div class="col-6" id="detailsTable_1">
+                                    <table class="table mt-3">
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">Release Date</th>
+                                                <td>10-12-2020</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Platform</th>
+                                                <td>PlayStation 4</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="col" id="detailsTable_2">
+                                    <table class="table mt-3" id="detailsTable2">
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">Age Restriction</th>
+                                                <td>18+</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Others</th>
+                                                <td>This game is extremely buggy, has terrible performance on consoles and low/medium end PC's and is lacking comparing to what was presented before release</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </section>
                             <!-- </div> -->
                         </div>
                         <div class="tab-pane fade" id="nav-reviews" role="tabpanel" aria-labelledby="nav-reviews-tab">
                             <!-- <div class="col"> -->
-                                <section class="ps-5 pe-5" id="productReviews">
-                                    <h3 class="text-start mt-4">Reviews</h3>
-                                </section>
+                            <section class="ps-5 pe-5" id="productReviews">
+                                <h3 class="text-start mt-4">Reviews</h3>
+                            </section>
                             <!-- </div> -->
                         </div>
                     </div>
