53ec2afc80606815bbc09402847d2e318ec65da8
up201806531 <up201806531@fe.up.pt>
2021-03-02 09:37:34 +0000

    Added bootstrap columns to button

--- cart.php
+++ cart.php
@@ -4,9 +4,9 @@
     <div style="font-size: 2.5rem; grid-row: 1;">
         <a>Your cart: </a>
     </div>
-
-    <div class="col-8 offset-md-1">
-        <div class="container pt-4 pb-4 overflow-auto rounded" style="background-color: lightgray; max-height: 75vh;">
+    
+    <div class="col-12">
+        <div class="container pt-4 pb-4 overflow-auto rounded col-8 offset-md-1" style="background-color: lightgray; max-height: 75vh;">
             <!-- Content here -->
             <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
                 <div class="card-body">
@@ -57,12 +57,15 @@
                 </div>
             </div>
         </div>
+        <div class="container offset-md-9">
+            <div class="col position-absolute position-absolute top-50" style="transform: translateX(35%);text-align: center;">
+                <p class="d-inline" style="font-size:1.5em">Total:</p>
+                <p class="d-inline" style="font-size:1.5em">47:97€</p>
+                <p></p>
+                <button type="button" class="btn btn-success btn-lg">Buy all</button>
+            </div>
+        </div>
     </div>
-    <div class="col position-absolute position-absolute top-50" style="transform: translateX(35%);text-align: center;">
-        <p class="d-inline" style="font-size:1.5em">Total:</p>
-        <p class="d-inline" style="font-size:1.5em">47:97€</p>
-        <p></p>
-        <button type="button" class="btn btn-success btn-lg">Buy all</button>
-    </div>
+    
 
 <?php include_once('footer.html'); ?>
