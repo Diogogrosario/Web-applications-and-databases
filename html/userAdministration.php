76540f436ab38ac4b003fca6d1a69bd29852ec88
Henrique <gamingwithapotato@gmail.com>
2021-03-05 17:11:12 +0000

    Improvements to user management

--- html/userAdministration.php
+++ html/userAdministration.php
@@ -3,98 +3,163 @@
 
 <div class="col">
     <div class="container">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
         <div class="row">
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
+                        <th scope="col" class="d-none d-md-table-cell">#</th>
+                        <th scope="col" class="d-none d-md-table-cell">Name</th>
                         <th scope="col">Username</th>
-                        <th scope="col">Email</th>
+                        <th scope="col" class="d-none d-md-table-cell">Email</th>
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
+                        <th scope="row" class="d-none d-md-table-cell">1</th>
+                        <td class="d-none d-md-table-cell">Mark</td>
                         <td>Otto</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button>
+                        </td>
                     </tr>
                     <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
+                        <th scope="row" class="d-none d-md-table-cell">2</th>
+                        <td class="d-none d-md-table-cell">Jacob</td>
                         <td>Thornton</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline"> Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
+                        <th scope="row" class="d-none d-md-table-cell">3</th>
+                        <td class="d-none d-md-table-cell">Larry</td>
                         <td>the Bird</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">4</th>
-                        <td>Henrique</td>
+                        <th scope="row" class="d-none d-md-table-cell">4</th>
+                        <td class="d-none d-md-table-cell">Henrique</td>
                         <td>Padoru</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">5</th>
-                        <td>Davide</td>
+                        <th scope="row" class="d-none d-md-table-cell">5</th>
+                        <td class="d-none d-md-table-cell">Davide</td>
                         <td>WaffleH</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">6</th>
-                        <td>Diogo</td>
+                        <th scope="row" class="d-none d-md-table-cell">6</th>
+                        <td class="d-none d-md-table-cell">Diogo</td>
                         <td>Ferno</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">7</th>
-                        <td>João</td>
+                        <th scope="row" class="d-none d-md-table-cell">7</th>
+                        <td class="d-none d-md-table-cell">João</td>
                         <td>Chalche</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">8</th>
-                        <td>João</td>
+                        <th scope="row" class="d-none d-md-table-cell">8</th>
+                        <td class="d-none d-md-table-cell">João</td>
                         <td>IraoDasForcas</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                     <tr>
-                        <th scope="row">9</th>
-                        <td>Eduardo</td>
+                        <th scope="row" class="d-none d-md-table-cell">9</th>
+                        <td class="d-none d-md-table-cell">Eduardo</td>
                         <td>Chequelo</td>
-                        <td>placeHolder@hotmail.com</td>
+                        <td class="d-none d-md-table-cell">placeHolder@hotmail.com</td>
                         <td>
-                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i> Ban</button>
-                            <button type="button" class="btn btn-success"> <i class="bi bi-person-check-fill"></i> Promote to Admin</button> </td>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-person-x-fill"></i>
+                                <div class="d-none d-md-inline">Ban
+                                </div>
+                            </button>
+                            <button type="button" class="btn btn-success"> <i class="fas fa-user-graduate"></i>
+                                <div class="d-none d-md-inline"> Promote to Admin
+                                </div>
+                            </button> </td>
                     </tr>
                 </tbody>
             </table>
