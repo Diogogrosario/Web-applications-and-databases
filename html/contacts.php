7f82d6ce16e0c2e75e85c16eca3169a74c93f814
Henrique <gamingwithapotato@gmail.com>
2021-03-06 19:57:57 +0000

    Improved contacts.php

--- html/contacts.php
+++ html/contacts.php
@@ -5,7 +5,7 @@
 
 <div class="container col-12 p-0" style="background-color: #f2f2f2">
 	<div class="col-12 offset-md-1 col-md-10 h-100 p-0" style="background-color:white; max-width: 100%">
-		<div class="container pt-3 px-5">
+		<div class="container" id="contact">
 			<div class="d-flex row">
 				<div class="col-12 col-md-8">
 					<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed">
@@ -19,7 +19,7 @@
 							125 Cool Street<br>
 							Porto<br>
 							Porto<br>
-							<abbr title="Phone">P:</abbr> +0351 123 456 789
+							Phone: +0351 123 456 789
 						</address>
 					</div>
 				</section>
