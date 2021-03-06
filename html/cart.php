7e38f93ae25173ec14cd98d062238099f521e3f6
up201806531 <up201806531@fe.up.pt>
2021-03-06 14:56:10 +0000

    Removed scrollbar.

--- html/cart.php
+++ html/cart.php
@@ -6,7 +6,7 @@
         <a>Your cart: </a>
     </div>
     
-    <div class="container pt-4 pb-4 overflow-auto rounded col-lg-9 col-12" style="background-color: lightgray; max-height: 75vh; margin: auto; max-width: 92%;">
+    <div id="cartScrollElem" class="container pt-4 pb-4 overflow-auto rounded col-lg-9 col-12" style="background-color: lightgray; max-height: 75vh; margin: auto; max-width: 92%;">
         <!-- Content here -->
         <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
             <div class="card-body">
@@ -14,7 +14,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div>
         <div class="card mx-auto mt-1 mb-1" style="width: 90%;">
@@ -23,7 +23,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div><div class="card mx-auto mt-1 mb-1" style="width: 90%;">
             <div class="card-body">
@@ -31,7 +31,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div><div class="card mx-auto mt-1 mb-1" style="width: 90%;">
             <div class="card-body">
@@ -39,7 +39,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div><div class="card mx-auto mt-1 mb-1" style="width: 90%;">
             <div class="card-body">
@@ -47,7 +47,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div><div class="card mx-auto mt-1 mb-1" style="width: 90%;">
             <div class="card-body">
@@ -55,7 +55,7 @@
                 <h5 class="card-text float-end fs-3" style="padding-right: 5%">x25</h5>
                 <p class="card-text">Some quick example text to build on the card.</p>
                 <a href="#" class="btn btn-danger pt-10" style="display: inline-block;"> <i class="d-inline bi bi-cart-x-fill"></i> <p class="d-sm-inline d-none">Remove from cart</p></a>
-                <p class="card-text float-end fs-3">1999000.99€</p>
+                <p class="d-inline float-end card-text fs-3">€</p><p class="d-inline card-text float-end fs-3">1999000.99</p>
             </div>
         </div>
     </div>
