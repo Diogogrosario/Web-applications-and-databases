03a7ed07793e92945c8896a1be5ee317b5bc5885
Henrique <gamingwithapotato@gmail.com>
2021-03-06 12:54:39 +0000

    Made search page more realistic and improves sidebar filter

--- html/searchResults.php
+++ html/searchResults.php
@@ -8,25 +8,29 @@
             <div class="container">
                 <ul class="list-group">
                     <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 col-md-3">
-                            <a class="item-card" href="./item.php">
-                                <img src="images/phone.jpg" class="card-img-top" alt="..." style="height:100%;">
+                        <div class="col-lg-3 zoom">
+                            <a class="item-card z" href="./item.php">
+                                <img src="images/computers/pc.jpg" class="card-img-top" alt="Asus Computer" style="height:100%;">
                             </a>
                         </div>
-                        <div class="col border-left border-dark">
+                        <div class="col-lg-9 border-left border-dark">
                             <div class="row">
-                                <div class="col-sm-6 col-8">
+                                <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
                                         <h2 class="title">Asus computer</h2>
-                                        <p class="text">This asus computer is the best for it's price!</p>
-                                        <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        <section class="itemInfo">
+                                            <p class="text">This asus computer is the best for it's price!</p>
+                                            <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        </section>
                                     </a>
                                 </div>
-                                <div class="col">
-                                    <div class="flex-row">
-                                        <span class="title fs-2">299.99€ </span>
-                                        <span class="title text-decoration-line-through">359.99€</span>
-                                        <span class="title">20% off!</span>
+                                <div class="col-sm-6 col-4">
+                                    <div class="d-flex justify-content-center">
+                                        <span class="title fs-2">300€ </span>
+                                        <small>
+                                            <span class="title text-decoration-line-through">360€</span>
+                                            <span class="title">20% off!</span>
+                                        </small>
                                     </div>
                                     <div class="d-flex justify-content-center">
                                         <button type="button" class="btn btn-success">Add to cart!</button>
@@ -36,25 +40,25 @@
                         </div>
                     </li>
                     <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 col-md-3">
-                            <a class="item-card" href="./item.php">
-                                <img src="images/spidercat.png" class="card-img-top" alt="..." style="height:100%;">
+                        <div class="col-lg-3 zoom">
+                            <a class="item-card z" href="./item.php">
+                                <img src="images/computers/asusRog.jpg" class="card-img-top" alt="Asus ROG" style="height:100%;">
                             </a>
                         </div>
-                        <div class="col border-left border-dark">
+                        <div class="col-lg-9 border-left border-dark">
                             <div class="row">
-                                <div class="col-sm-6 col-8">
+                                <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus computer</h2>
-                                        <p class="text">This asus computer is the best for it's price!</p>
-                                        <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        <h2 class="title">Asus Rog</h2>
+                                        <section class="itemInfo">
+                                            <p class="text">This asus rog is going to make you better at any game!</p>
+                                            <p class="text"><small class="text-muted">Last updated 10 mins ago</small></p>
+                                        </section>
                                     </a>
                                 </div>
-                                <div class="col">
-                                    <div class="flex-row">
-                                        <span class="title fs-2">299.99€ </span>
-                                        <span class="title text-decoration-line-through">359.99€</span>
-                                        <span class="title">20% off!</span>
+                                <div class="col-sm-6 col-4 h-100">
+                                    <div class="d-flex justify-content-center">
+                                        <span class="title fs-2">2000€ </span>
                                     </div>
                                     <div class="d-flex justify-content-center">
                                         <button type="button" class="btn btn-success">Add to cart!</button>
@@ -64,81 +68,25 @@
                         </div>
                     </li>
                     <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 col-md-3">
-                            <a class="item-card" href="./item.php">
-                                <img src="images/pc.jpg" class="card-img-top" alt="..." style="height:100%;">
+                        <div class="col-lg-3 zoom ">
+                            <a class="item-card z" href="./item.php">
+                                <img src="images/cyberpunk_1.jpg" class="card-img-top " alt="Cuber Punk" style="height:100%;">
                             </a>
                         </div>
-                        <div class="col border-left border-dark">
+                        <div class="col-lg-9 border-left border-dark">
                             <div class="row">
-                                <div class="col-sm-6 col-8">
+                                <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus computer</h2>
-                                        <p class="text">This asus computer is the best for it's price!</p>
-                                        <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        <h2 class="title">Cyber Punk</h2>
+                                        <section class="itemInfo">
+                                            <p class="text">Cyber punk is a futuristic RPG that will blow you away with stuning graphics!</p>
+                                            <p class="text"><small class="text-muted">Last updated 2 days ago</small></p>
+                                        </section>
                                     </a>
                                 </div>
-                                <div class="col">
-                                    <div class="flex-row">
-                                        <span class="title fs-2">299.99€ </span>
-                                        <span class="title text-decoration-line-through">359.99€</span>
-                                        <span class="title">20% off!</span>
-                                    </div>
+                                <div class="col-sm-6 col-4 h-100">
                                     <div class="d-flex justify-content-center">
-                                        <button type="button" class="btn btn-success">Add to cart!</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 col-md-3">
-                            <a class="item-card" href="./item.php">
-                                <img src="images/pc.jpg" class="card-img-top" alt="..." style="height:100%;">
-                            </a>
-                        </div>
-                        <div class="col border-left border-dark">
-                            <div class="row">
-                                <div class="col-sm-6 col-8">
-                                    <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus computer</h2>
-                                        <p class="text">This asus computer is the best for it's price!</p>
-                                        <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                    </a>
-                                </div>
-                                <div class="col">
-                                    <div class="flex-row">
-                                        <span class="title fs-2">299.99€ </span>
-                                        <span class="title text-decoration-line-through">359.99€</span>
-                                        <span class="title">20% off!</span>
-                                    </div>
-                                    <div class="d-flex justify-content-center">
-                                        <button type="button" class="btn btn-success">Add to cart!</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 col-md-3">
-                            <a class="item-card" href="./item.php">
-                                <img src="images/pc.jpg" class="card-img-top" alt="..." style="height:100%;">
-                            </a>
-                        </div>
-                        <div class="col border-left border-dark">
-                            <div class="row">
-                                <div class="col-sm-6 col-8">
-                                    <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus computer</h2>
-                                        <p class="text">This asus computer is the best for it's price!</p>
-                                        <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                    </a>
-                                </div>
-                                <div class="col">
-                                    <div class="flex-row">
-                                        <span class="title fs-2">299.99€ </span>
-                                        <span class="title text-decoration-line-through">359.99€</span>
-                                        <span class="title">20% off!</span>
+                                        <span class="title fs-2">59€ </span>
                                     </div>
                                     <div class="d-flex justify-content-center">
                                         <button type="button" class="btn btn-success">Add to cart!</button>
