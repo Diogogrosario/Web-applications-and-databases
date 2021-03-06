38545f0766ea80d33f9d8dcce2dad87a7cb0178f
Henrique <gamingwithapotato@gmail.com>
2021-03-06 21:49:42 +0000

    Improvements done to search results

--- html/searchResults.php
+++ html/searchResults.php
@@ -6,18 +6,18 @@
     <div class="content" style="padding:2%">
         <div class="row">
             <div class="container">
-                <ul class="list-group">
-                    <li class="row border-bottom border-dark pb-3 mt-3">
+                <ul class="list-group" id="itemsList">
+                    <li class="row pb-3 mt-3">
                         <div class="col-lg-3 zoom">
                             <a class="item-card z" href="./item.php">
-                                <img src="images/computers/pc.jpg" class="card-img-top" alt="Asus Computer" style="height:100%;">
+                                <img src="images/computers/pc.jpg" class="card-img-top mx-auto d-flex" id="searchResultItemImage" alt="Asus Computer">
                             </a>
                         </div>
                         <div class="col-lg-9 border-left border-dark">
                             <div class="row">
                                 <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus computer</h2>
+                                        <h3 class="title">Asus computer</h3>
                                         <section class="itemInfo">
                                             <p class="text">This asus computer is the best for it's price!</p>
                                             <p class="text"><small class="text-muted">Last updated 3 mins ago</small></p>
@@ -26,16 +26,21 @@
                                 </div>
                                 <div class="col-sm-6 col-4">
                                     <div class="d-flex justify-content-center">
-                                        <span class="title fs-2">300€ </span>
+                                        <span class="title fs-3">300€ </span>
                                         <small>
                                             <span class="title text-decoration-line-through">360€</span>
                                             <span class="title">20% off!</span>
                                         </small>
                                     </div>
 
-                                    <div class="d-flex justify-content-center">
+                                    <div class="d-flex justify-content-center pb-2">
                                         <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#balanceModal">
                                             Add to cart!
+                                        </button>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn btn-danger">
+                                            Add to wishlist!
                                         </button>
                                     </div>
 
@@ -66,17 +71,17 @@
                             </div>
                         </div>
                     </li>
-                    <li class="row border-bottom border-dark pb-3 mt-3">
+                    <li class="row pb-3 mt-3">
                         <div class="col-lg-3 zoom">
                             <a class="item-card z" href="./item.php">
-                                <img src="images/computers/asusRog.jpg" class="card-img-top" alt="Asus ROG" style="height:100%;">
+                                <img src="images/computers/asusRog.jpg" class="card-img-top mx-auto d-flex" id="searchResultItemImage" alt="Asus ROG">
                             </a>
                         </div>
                         <div class="col-lg-9 border-left border-dark">
                             <div class="row">
                                 <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
-                                        <h2 class="title">Asus Rog</h2>
+                                        <h3 class="title">Asus Rog</h3>
                                         <section class="itemInfo">
                                             <p class="text">This asus rog is going to make you better at any game!</p>
                                             <p class="text"><small class="text-muted">Last updated 10 mins ago</small></p>
@@ -85,12 +90,17 @@
                                 </div>
                                 <div class="col-sm-6 col-4 h-100">
                                     <div class="d-flex justify-content-center">
-                                        <span class="title fs-2">2000€ </span>
+                                        <span class="title fs-3">2000€ </span>
                                     </div>
-                                    <div class="d-flex justify-content-center">
-                                        <div class="d-flex justify-content-center">
+
+                                    <div class="d-flex justify-content-center pb-2">
                                         <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#balanceModal">
                                             Add to cart!
+                                        </button>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn btn-danger">
+                                            Add to wishlist!
                                         </button>
                                     </div>
 
@@ -116,22 +126,21 @@
                                             </div>
                                         </div>
                                     </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </li>
-                    <li class="row border-bottom border-dark pb-3 mt-3">
-                        <div class="col-lg-3 zoom ">
+                    <li class="row pb-3 mt-3">
+                        <div class="col-lg-3 zoom">
                             <a class="item-card z" href="./item.php">
-                                <img src="images/cyberpunk_1.jpg" class="card-img-top " alt="Cuber Punk" style="height:100%;">
+                                <img src="images/cyberpunk_1.jpg" class="card-img-top mx-auto d-flex" id="searchResultItemImage" alt="Cuber Punk">
                             </a>
                         </div>
                         <div class="col-lg-9 border-left border-dark">
                             <div class="row">
                                 <div class="col-sm-6 col-8 ps-5">
                                     <a class="item-card" href="./item.php">
-                                        <h2 class="title">Cyber Punk</h2>
+                                        <h3 class="title">Cyber Punk</h3>
                                         <section class="itemInfo">
                                             <p class="text">Cyber punk is a futuristic RPG that will blow you away with stuning graphics!</p>
                                             <p class="text"><small class="text-muted">Last updated 2 days ago</small></p>
@@ -140,12 +149,16 @@
                                 </div>
                                 <div class="col-sm-6 col-4 h-100">
                                     <div class="d-flex justify-content-center">
-                                        <span class="title fs-2">59€ </span>
+                                        <span class="title fs-3">59€ </span>
                                     </div>
-                                    <div class="d-flex justify-content-center">
-                                        <div class="d-flex justify-content-center">
+                                    <div class="d-flex justify-content-center pb-2">
                                         <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#balanceModal">
                                             Add to cart!
+                                        </button>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn btn-danger">
+                                            Add to wishlist!
                                         </button>
                                     </div>
 
@@ -170,7 +183,6 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
                                     </div>
                                 </div>
                             </div>
