873d8e92e4a85f30622cf53c2d61866fa242074b
Henrique <gamingwithapotato@gmail.com>
2021-03-07 16:37:05 +0000

    Improvements to authentication pages

--- html/signup.php
+++ html/signup.php
@@ -2,35 +2,45 @@
 <?php include_once('sidebarItem.html'); ?>
 
 <!-- Form -->
-<div>
-    <div style="text-align: center; padding-top: 7%; font-size: 2.2em">
-        <b>Account info</b>
-    </div>
-    <div style="margin: auto; width: 80%; padding-top: 3%; font-size: 1.2rem;">
-        <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>E-mail</b></label>
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="e-mail">
-    </div>
-    <div style="margin: auto; width: 80%; padding-top: 3%; font-size: 1.2rem;">
-        <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Username</b></label>
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="username">
-    </div>
-    <div style="margin: auto; width: 80%; padding-top: 2%; font-size: 1.2rem;">
-        <label style="padding-bottom: 1%;" for="formGroupExampleInput2" class="form-label"><b>Password</b></label>
-        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="password">
-    </div>
-    <div style="text-align: center; padding-top: 7%; font-size: 2.2em">
-        <b>Personal info</b>
-    </div>
-    <div style="margin: auto; width: 80%; padding-top: 3%; font-size: 1.2rem;">
-        <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Adress, door number</b></label>
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Eg: Cool Street, 320">
-    </div>
-    <div style="margin: auto; width: 80%; padding-top: 3%; font-size: 1.2rem;">
-        <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Postal code</b></label>
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Eg: 1234-567">
-    </div>
-    <div style="text-align: center; padding-bottom: 8vh">
-        <button type="button" class="btn btn-primary" style="margin-top: 5%; font-size: 2em;">Sign up</button>
+
+<div class="p-0" style="background-color:#f2f2f2;">
+    <div class="container col-md-7 p-lg-5 p-3 shadow-sm h-100" style="background-color:white">
+        <div class="w-75 m-auto pt-3">
+            <div class="text-center pt-3 fs-3">
+                <b>Account info</b>
+            </div>
+            <div class="pt-3">
+                <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>E-mail</b></label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="E-mail">
+            </div>
+            <div class="row pt-3">
+                <div class="col-md-6">
+                    <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>First Name</b></label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="First Name">
+                </div>
+                <div class="col-md-6">
+                    <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Last Name</b></label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Last Name">
+                </div>
+            </div>
+            <div class="pt-3">
+                <label style="padding-bottom: 1%;" for="formGroupExampleInput" class="form-label"><b>Username</b></label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username">
+            </div>
+            <div class="pt-3">
+                <label style="padding-bottom: 1%;" for="formGroupExampleInput2" class="form-label"><b>Password</b></label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password">
+            </div>
+            <div class="pt-3">
+                <label style="padding-bottom: 1%;" for="formGroupExampleInput2" class="form-label"><b>Confirm Password</b></label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Confirm Password">
+            </div>
+
+            <div style="text-align: center; padding-bottom: 8vh">
+                <button type="button" class="btn btn-dark" style="margin-top: 5%; font-size: 2em;">Sign up</button>
+            </div>
+        </div>
     </div>
 </div>
+
 <?php include_once('footer.html'); ?>
