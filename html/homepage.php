8f7dea85bf50531139d3a438ba476784d0bf3312
Henrique <gamingwithapotato@gmail.com>
2021-03-07 17:10:20 +0000

    Added new images to main page

--- html/homepage.php
+++ html/homepage.php
@@ -9,17 +9,13 @@
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselProductImages" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner" style="width:100%; height:20vh">
                         <div class="carousel-item active">
                             <img src="images/wantMoreSales.jpg" class="img-fluid" alt="More Sales" style='height:20vh; width: 100%; object-fit: contain'>
                         </div>
                         <div class="carousel-item">
-                            <img src="images/shrek2PS2_1.png" class="d-block img-fluid mx-auto" style='height:20vh; width: 100%; object-fit: contain' alt="Shrek" style="max-height:40vh;">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/shrek2PS2_2.png" class="d-block img-fluid mx-auto" style='height:20vh; width: 100%; object-fit: contain' alt="Shrek" style="max-height:40vh;">
+                            <img src="images/wantMoreSales2.jpg" class="d-block img-fluid mx-auto" style='height:20vh; width: 100%; object-fit: contain' alt="Shrek" style="max-height:40vh;">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductImages" data-bs-slide="prev">
