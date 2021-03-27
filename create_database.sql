4b6332f3fa42e59c3686e60b322432054c4d1877
Henrique <gamingwithapotato@gmail.com>
2021-03-27 16:30:14 +0000

    Updated db

--- create_database.sql
+++ create_database.sql
@@ -1,16 +1,39 @@
-DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS category_detail CASCADE;
+DROP TABLE IF EXISTS item_detail CASCADE;
+DROP TABLE IF EXISTS apply_discount CASCADE;
+DROP TABLE IF EXISTS stock_notification CASCADE;
+DROP TABLE IF EXISTS discount_notification CASCADE;
+DROP TABLE IF EXISTS notification CASCADE;
+DROP TABLE IF EXISTS discount CASCADE;
+DROP TABLE IF EXISTS wishlist CASCADE;
+DROP TABLE IF EXISTS cart CASCADE;
+DROP TABLE IF EXISTS item_photo CASCADE;
+DROP TABLE IF EXISTS advertisement CASCADE;
+DROP TABLE IF EXISTS purchase_item CASCADE;
+DROP TABLE IF EXISTS purchase CASCADE;
+DROP TABLE IF EXISTS ban CASCADE;
+DROP TABLE IF EXISTS item CASCADE;
+DROP TABLE IF EXISTS details CASCADE;
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS authenticated CASCADE;
+DROP TABLE IF EXISTS admins CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS address CASCADE;
+DROP TABLE IF EXISTS photo CASCADE;
+DROP TABLE IF EXISTS country CASCADE;
+
 CREATE TABLE country (
     country_id SERIAL PRIMARY KEY,
     name text NOT NULL CONSTRAINT country_name_uk UNIQUE
 ); 
 
-DROP TABLE IF EXISTS photo;
+
 CREATE TABLE photo (
     photo_id SERIAL PRIMARY KEY,
     path text NOT NULL 
 );
 
-DROP TABLE IF EXISTS address;
 CREATE TABLE address (
     address_id SERIAL PRIMARY KEY,
     city text NOT NULL,
@@ -19,7 +42,6 @@ CREATE TABLE address (
     country_id INTEGER REFERENCES country(country_id) ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username text NOT NULL CONSTRAINT username_uk UNIQUE,
@@ -28,23 +50,20 @@ CREATE TABLE users (
     last_name text NOT NULL,
     password text NOT NULL,
     deleted BOOLEAN DEFAULT FALSE,
-    img INTEGER REFERENCES photo(photoID) ON UPDATE CASCADE,
-    billingAddress INTEGER REFERENCES address(addressID) ON UPDATE CASCADE,
-    shippingAddress INTEGER REFERENCES address(addressID) ON UPDATE CASCADE
+    img INTEGER REFERENCES photo(photo_id) ON UPDATE CASCADE,
+    billing_address INTEGER REFERENCES address(address_id) ON UPDATE CASCADE,
+    shipping_address INTEGER REFERENCES address(address_id) ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS admin;
-CREATE TABLE admin (
+CREATE TABLE admins (
     admin_id INTEGER REFERENCES users(user_id) ON UPDATE CASCADE PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS authenticated;
 CREATE TABLE authenticated (
-    authenticated_id INTEGER REFERENCES user(userID) ON UPDATE CASCADE PRIMARY KEY,
+    authenticated_id INTEGER REFERENCES users(user_id) ON UPDATE CASCADE PRIMARY KEY,
     balance money DEFAULT 0 NOT NULL
 );
  
-DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     review_id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES authenticated(authenticated_id) ON UPDATE CASCADE,
@@ -53,19 +72,16 @@ CREATE TABLE review (
     rating INTEGER NOT NULL CONSTRAINT rating_ck CHECK (((rating > 0) AND (rating <= 5)))
 );
 
-DROP TABLE IF EXISTS category;
 CREATE TABLE category (
     category_id SERIAL PRIMARY KEY,
     name text NOT NULL UNIQUE
 );
  
-DROP TABLE IF EXISTS details;
 CREATE TABLE detail (
     detail_id SERIAL PRIMARY KEY,
     name text NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS item;
 CREATE TABLE item (
     item_id SERIAL PRIMARY KEY,
     name text NOT NULL,
@@ -77,32 +93,28 @@ CREATE TABLE item (
     category_id INTEGER REFERENCES category (category_id) ON UPDATE CASCADE
 );
  
-DROP TABLE IF EXISTS ban;
 CREATE TABLE ban (
-    admin_id INTEGER NOT NULL REFERENCES admin(admin_id) ON UPDATE CASCADE,
-    user_id INTEGER NOT NULL REFERENCES user (user_id) ON UPDATE CASCADE,
+    admin_id INTEGER NOT NULL REFERENCES admins(admin_id) ON UPDATE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE,
     "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     reason text NOT NULL,
-    PRIMARY KEY (adminID, userID)
+    PRIMARY KEY (admin_id, user_id)
 );
 
-DROP TABLE IF EXISTS purchase;
 CREATE TABLE purchase (
     purchase_id SERIAL PRIMARY KEY,
     userID INTEGER REFERENCES authenticated (authenticated_id) ON UPDATE CASCADE,
     "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
 );
  
-DROP TABLE IF EXISTS purchase_item;
 CREATE TABLE purchase_item (
     purchase_id INTEGER NOT NULL REFERENCES purchase (purchase_id) ON UPDATE CASCADE,
-    itemID INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
+    item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
     price MONEY NOT NULL,
     quantity INTEGER NOT NULL CONSTRAINT quantity_more_zero CHECK (quantity > 0),
     PRIMARY KEY (purchase_id, item_id)
 );
 
-DROP TABLE IF EXISTS advertisement;
 CREATE TABLE advertisement (
     advertisement_id INTEGER PRIMARY KEY,
     title text NOT NULL,
@@ -112,14 +124,13 @@ CREATE TABLE advertisement (
     CONSTRAINT ad_dates_ck CHECK (begin_date < end_date)
 );
 
-DROP TABLE IF EXISTS item_photo;
 CREATE TABLE item_photo (
     photo_id INTEGER NOT NULL REFERENCES photo (photo_id) ON UPDATE CASCADE PRIMARY KEY,
     item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE
 
 );
  
-DROP TABLE IF EXISTS cart;
+
 CREATE TABLE cart (
     user_id INTEGER REFERENCES authenticated (authenticated_id) ON UPDATE CASCADE,
     item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
@@ -128,48 +139,42 @@ CREATE TABLE cart (
     PRIMARY KEY (user_id, item_id)
 );
  
-DROP TABLE IF EXISTS wishlist;
+
 CREATE TABLE wishlist (
     user_id INTEGER REFERENCES authenticated (authenticated_id) ON UPDATE CASCADE,
     item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
     add_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-    PRIMARY KEY (userID, itemID)
+    PRIMARY KEY (user_id, item_id)
 );
 
-DROP TABLE IF EXISTS discount;
 CREATE TABLE discount (
     discount_id SERIAL PRIMARY KEY,
     "percentage" INTEGER NOT NULL CONSTRAINT valid_percentage CHECK ((("percentage" > 0) AND ("percentage" <= 100))),
     begin_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     end_date TIMESTAMP WITH TIME zone NOT NULL,
-    CONSTRAINT ad_dates_ck CHECK (beginDate < endDate)
+    CONSTRAINT ad_dates_ck CHECK (begin_date < end_date)
 );
 
-DROP TABLE IF EXISTS "notification";
 CREATE TABLE "notification" (
     notification_id SERIAL PRIMARY KEY
     
 );
 
-DROP TABLE IF EXISTS discount_notification;
 CREATE TABLE discount_notification (
     notification_id INTEGER NOT NULL REFERENCES discount (discount_id) ON UPDATE CASCADE PRIMARY KEY,
     discount_id INTEGER NOT NULL REFERENCES discount (discount_id) ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS stock_notification;
 CREATE TABLE stock_notification (
     notification_id SERIAL PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS apply_discount;
 CREATE TABLE apply_discount (
     item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
     discount_id INTEGER NOT NULL REFERENCES discount (discount_id) ON UPDATE CASCADE,
-    PRIMARY KEY (itemID, discountID)
+    PRIMARY KEY (item_id, discount_id)
 );
 
-DROP TABLE IF EXISTS item_detail;
 CREATE TABLE itemDetail (
     item_id INTEGER NOT NULL REFERENCES item (item_id) ON UPDATE CASCADE,
     detail_id INTEGER NOT NULL REFERENCES detail (detail_id) ON UPDATE CASCADE,
@@ -177,7 +182,6 @@ CREATE TABLE itemDetail (
     PRIMARY KEY (detail_id, item_id)
 );
 
-DROP TABLE IF EXISTS category_detail;
 CREATE TABLE category_detail (
     category_id INTEGER NOT NULL REFERENCES category (category_id) ON UPDATE CASCADE,
     detail_id INTEGER NOT NULL REFERENCES detail (detail_id) ON UPDATE CASCADE,
