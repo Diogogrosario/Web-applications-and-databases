72fa45ccec5fb95b3c218750038dc2e61f9f5fb2
Henrique <gamingwithapotato@gmail.com>
2021-04-26 18:31:52 +0100

    Improvementes to the review partial

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|string|max:255|unique:users',
+            'username' => 'required|string|max:20|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'first_name' => 'required|string|max:255',
