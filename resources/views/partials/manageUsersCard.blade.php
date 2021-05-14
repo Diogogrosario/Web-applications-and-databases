b7eea454b0f0bb2160f704337ee3426ed079f29a
Henrique <gamingwithapotato@gmail.com>
2021-05-14 19:33:27 +0100

    Fixed errors on user management page

--- resources/views/partials/manageUsersCard.blade.php
+++ resources/views/partials/manageUsersCard.blade.php
@@ -2,7 +2,7 @@
     <th scope="row" class="d-none d-lg-table-cell">{{ $key + 1 }}</th>
     <td>{{ $user["first_name"] }}</td>
     <td>
-         <a id="username-anchor"href={{ "/userProfile/" . $user["user_id"] }}>{{ $user["username"] }} </a>
+         <a class="username-anchor" href={{ "/userProfile/" . $user["user_id"] }}>{{ $user["username"] }} </a>
     </td>
     <td class="d-none d-lg-table-cell">{{ $user["email"] }}</td>
         @if (!$user->isBanned())    
