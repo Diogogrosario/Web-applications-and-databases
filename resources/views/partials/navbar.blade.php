f4f41c934ba1f3fb46845dadf356080c565ae63d
Henrique <gamingwithapotato@gmail.com>
2021-05-30 18:22:06 +0100

    added circle to show user has notifications

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -54,7 +54,7 @@
               <ul class="navbar-nav mb-2 mb-lg-0">
                   <?php if($logged_in){ $numberInCart = Auth::user()->cartTotalNumber();?>
                       <li class="nav-item d-lg-flex align-items-lg-center py-2 py-lg-0 px-lg-2">
-                            <a class="nav-link pe-0" href={{"/userProfile/cart"}}>
+                          <a class="nav-link pe-0" href={{"/userProfile/cart"}}>
                             <i class="bi bi-cart" style="font-size: 1.5em;"></i>
                             <span class="cart-number-small d-lg-none" style="color:white !important">
                                 <span class="cart-number">{{$numberInCart}}</span> items
@@ -76,19 +76,24 @@
                             <div class="d-flex align-items-center justify-content-center pt-1">
                                 <a href={{ "/userProfile/"}} class="d-block" style="width: 3vw;">
                                     @if (Auth::user()->image()->first())
-                                        <div id="profilePic" class="d-flex rounded-circle" style={{"height:0;width:100%;padding-bottom:100%;background-image:url(\"" . asset("/img/users/" . Auth::user()->image()->first()["path"]) . "\");background-position:center;background-size:cover;"}}>
-                                        </div>
+                                    <div id="profilePic" class="d-flex rounded-circle" style={{"height:0;width:100%;padding-bottom:100%;background-image:url(\"" . asset("/img/users/" . Auth::user()->image()->first()["path"]) . "\");background-position:center;background-size:cover;"}}>
+                                    </div>
+                                    
                                     @else
-                                        <div id="profilePic" class="d-flex rounded-circle" style={{"height:0;width:100%;padding-bottom:100%;background-image:url(\"" . asset("/img/users/default.png") . "\");background-position:center;background-size:cover;"}}>
-                                        </div>
+                                    <div id="profilePic" class="d-flex rounded-circle" style={{"height:0;width:100%;padding-bottom:100%;background-image:url(\"" . asset("/img/users/default.png") . "\");background-position:center;background-size:cover;"}}>
+                                    </div>
                                     @endif
                                     
                                 </a>
-                                <a id="navbarUsername" class="nav-link p-1 align-middle" href={{"/userProfile/"}} style="color:white !important; border-right: 1px solid;">{{ Auth::user()["username"] }}</a>
-                                @if (Auth::user()["is_admin"])
-                                <a class="nav-link p-1 align-middle" href={{"/management"}} style="color:white !important; border-right: 1px solid;" >Manage</a>
+                                @if (Auth::user()->unseenNotifications()->first())
+                                    <span id="hasNotifications" class=" d-lg-block rounded-circle">
+                                    </span>
                                 @endif
-                                <a class="nav-link p-1" href="/logout" style="color:white !important;">Logout</a>
+                                <a id="navbarUsername" class="nav-link p-1 pe-2 align-middle" href={{"/userProfile/"}} style="color:white !important; border-right: 1px solid;">{{ Auth::user()["username"] }}</a>
+                                @if (Auth::user()["is_admin"])
+                                <a class="nav-link p-1 align-middle px-2" href={{"/management"}} style="color:white !important; border-right: 1px solid;" >Manage</a>
+                                @endif
+                                <a class="nav-link p-1 ps-2" href="/logout" style="color:white !important;">Logout</a>
                             </div>
                       <?php } ?>
                   </li>
