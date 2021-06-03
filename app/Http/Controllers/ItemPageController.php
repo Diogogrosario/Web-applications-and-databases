93c466f41b5141101e15e535cff2eab8372b5a8b
Henrique <gamingwithapotato@gmail.com>
2021-06-03 15:36:16 +0100

    Name of item now appears on url

--- app/Http/Controllers/ItemPageController.php
+++ app/Http/Controllers/ItemPageController.php
@@ -17,7 +17,18 @@ class ItemPageController extends Controller
      */
     public function show($id)
     {
+        $exploded = explode("-",$id);
+
+        $id = $exploded[count($exploded)-1];
+
+        if(!is_numeric($id))
+            abort(404);
+
         $item = Item::findOrFail($id);
+
+        $url =  str_replace(" ","-",$item["name"]) . "-" . $id;
+
+        echo("<script>history.replaceState({},'','$url');</script>");
        
         $categories = Category::all()->sortBy("category_id");
         return view('pages.itemPage')->with('item', $item)->with("categories", $categories);
